ea113dda46d4faee49513710b8c5ed9b6c2583ee
zhuris <g.zhuris@gmail.com>
2019-11-19 12:35:00 +0200

    starter-kit

---
+++ JsTeam/src/scripts/index.js
@@ -0,0 +1,3 @@
+import '../styles/style.sass';
+
+document.write('Hello world!'); // REMOVE THIS IF DEVELOPMENT WAS STARTED
---
+++ JsTeam/webpack/webpack.config.dev.js
@@ -0,0 +1,46 @@
+const Path = require('path');
+const Webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    chunkFilename: 'js/[name].chunk.js'
+  },
+  devServer: {
+    inline: true,
+    hot: true,
+    contentBase: Path.resolve(__dirname, "../src"),
+    watchContentBase: true
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    new Webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        include: Path.resolve(__dirname, '../src'),
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: true
+        }
+      },
+      // {
+      //   test: /\.(js)$/,
+      //   include: Path.resolve(__dirname, '../src'),
+      //   loader: 'babel-loader'
+      // },
+      {
+        test: /\.s[ca]ss$/,
+        use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
+      }
+    ]
+  }
+});
